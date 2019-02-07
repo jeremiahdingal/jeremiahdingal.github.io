0e62e38878f1e401efc55d599faada87078c3d9f
Jeremiah japhet dingal <jeremiahjaphetdingal@Jeremiahs-MacBook-Air.local>
2019-02-08 00:53:27 +0800

    ‘a16’

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import './App.css';
 import { Controller, Scene } from 'react-scrollmagic';
 import { Tween, Timeline,SplitLetters } from 'react-gsap';
-import StickyBox from "react-sticky-box";
 
 
 
@@ -11,54 +10,39 @@ class App extends Component {
     return (
 
       <div className="App">
-          <StickyBox offsetTop={20} offsetBottom={20}><div className="panel"><span>wew</span></div></StickyBox>
 
         <div className="HeadImg">
-          <Controller globalSceneOptions={{ triggerHook: 'onLeave' }}>
-           <Scene      
-                    pin={true}
-                    reverse={true}
-                    duration={1000}
-                  >
-                    <Tween 
-                      wrapper={
-                        <div className="panel blue" />
-                      }
-                      staggerFrom= {{
-                         opacity: 0,
-                        ease: 'Expo.easeInOut',
-                      }}
-                      stagger={0.15}
-                    >
-                    
-                        <span >Hi.</span>
-                    </Tween>
-            </Scene>
-            <Scene      
-                    pin={true}
-                    reverse={true}
-                    duration={500}
-                  >
-                    <Tween 
-                      wrapper={
-                        <div className="panel turqoise" />
-                      }
-                      staggerFrom= {{
-                         opacity: 0,
-                        ease: 'Expo.easeInOut',
-                      }}
-                      stagger={0.15}
-                    >
-                      <span className="text">I am Jeremiah, <br/>and this is my folio.</span>
-                    </Tween>
-            </Scene>
-            <Scene pin>
-              <div className="panel green"><span>3</span></div>
-            </Scene>
-            <Scene pin>
-              <div className="panel bordeaux"><span>4</span></div>
-            </Scene>
-          </Controller>
+        <Controller>
+      <Scene
+        triggerHook="onLeave"
+        duration="300%"
+        pin
+      >
+        <Timeline
+          wrapper={<div id="pinContainer" />}
+        >
+          <section className="panel blue"><span>Panel</span></section>
+          <Tween
+            from={{ x: '-100%' }}
+            to={{ x: '0%' }}
+          >
+            <section className="panel turqoise"><span>Panel</span></section>
+          </Tween>
+          <Tween
+            from={{ x: '100%' }}
+            to={{ x: '0%' }}
+          >
+            <section className="panel green"><span>Panel</span></section>
+          </Tween>
+          <Tween
+            from={{ y: '-100%' }}
+            to={{ y: '0%' }}
+          >
+            <section className="panel bordeaux"><span>Panel</span></section>
+          </Tween>
+        </Timeline>
+      </Scene>
+    </Controller>
         </div>
       </div>
     );

54fdf86f11b1d0fd33e719d7ddeb31dbee5dec7a
Jeremiah japhet dingal <jeremiahjaphetdingal@Jeremiahs-MacBook-Air.local>
2019-02-07 20:26:14 +0800

    white site

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Window, TitleBar, Text } from 'react-desktop/macOs';
+import { Controller, Scene } from 'react-scrollmagic';
+import { Tween, Timeline } from 'react-gsap';
+
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+              
         </header>
+        <Controller>
+         <Scene duration={2000} triggerHook="onLeave" pin>
+         {(progress) => (
+            <Timeline totalProgress={progress} paused>
+            <Tween
+                from={{ x: '10%', top: '60%' }}
+                to={{ x: '60%', top: '10%' }}
+            >
+                  <div className="Win">
+                      <Window
+                      chrome
+                      height="300px"
+                      width="300px"
+                      padding="10px"
+                      >
+                        <TitleBar title="Portfolio" controls height="43"/>
+                        <Text>Hello World</Text>
+                      </Window>
+                      </div>
+
+            </Tween>  
+            </Timeline>
+
+          )}
+
+          </Scene>
+          <Scene duration={200} pin={{ pushFollowers: false }}>
+            <div className="sticky"><div>Pin Test</div></div>
+            
+          </Scene>
+        </Controller>
+        <div className="section" />
       </div>
     );
   }

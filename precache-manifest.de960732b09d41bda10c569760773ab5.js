729ea1d627f58b5e6bcc713ea3c66de854e6354c
Jeremiah japhet dingal <jeremiahjaphetdingal@Jeremiahs-MacBook-Air.local>
2019-02-07 23:22:15 +0800

    Updates

--- precache-manifest.de960732b09d41bda10c569760773ab5.js
+++ precache-manifest.de960732b09d41bda10c569760773ab5.js
@@ -8,19 +8,19 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "a6b795adf9837b547e3d",
-    "url": "/static/js/main.a6b795ad.chunk.js"
+    "revision": "5b003fc3f9a154254863",
+    "url": "/static/js/main.5b003fc3.chunk.js"
   },
   {
     "revision": "2c02f3683c71e62e4bba",
     "url": "/static/js/1.2c02f368.chunk.js"
   },
   {
-    "revision": "a6b795adf9837b547e3d",
-    "url": "/static/css/main.169efd27.chunk.css"
+    "revision": "5b003fc3f9a154254863",
+    "url": "/static/css/main.9c84bf22.chunk.css"
   },
   {
-    "revision": "17b7d54e4c6c674e73d7810b6f323444",
+    "revision": "00c1dce30c17970f2cc095bdc1131082",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f63fe42e118b957645012955bf5005f2.js"
+  "/precache-manifest.de960732b09d41bda10c569760773ab5.js"
 );
 
 workbox.clientsClaim();

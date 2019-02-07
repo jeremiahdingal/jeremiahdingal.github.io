bf2ea2b9e0e6ee0692546b799abe40afef070c20
Jeremiah japhet dingal <jeremiahjaphetdingal@Jeremiahs-MacBook-Air.local>
2019-02-07 22:32:40 +0800

    Updates

---
+++ precache-manifest.09c6f98d4476a3047a0b803d6900a27f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac75bc35647b966cfc060bd74b9a1ca7",
+    "url": "/static/media/Head.ac75bc35.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b65788d220332296e493",
+    "url": "/static/js/main.b65788d2.chunk.js"
+  },
+  {
+    "revision": "2c02f3683c71e62e4bba",
+    "url": "/static/js/1.2c02f368.chunk.js"
+  },
+  {
+    "revision": "b65788d220332296e493",
+    "url": "/static/css/main.8eac52a5.chunk.css"
+  },
+  {
+    "revision": "99781d5545cc87d9bddc1534d05f2d12",
+    "url": "/index.html"
+  }
+];

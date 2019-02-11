b675b2b89c3a14b9e07696ee855688b8c666bf30
Jeremiah japhet dingal <jeremiahjaphetdingal@Jeremiahs-MacBook-Air.local>
2019-02-11 19:43:16 +0800

    ‘b1’

--- src/EtcModal.js
+++ src/EtcModal.js
@@ -5,14 +5,32 @@ import duracretemixer from './assets/duracretemixer.png'
 import duracreteposter from './assets/duracrete poster.png'
 import parcpage from './assets/parcpage2.png'
 import banners from './assets/banners.png'
+import Carousel from 'react-bootstrap/Carousel'
 
 
 class EtcModal extends Component {
+  constructor(props, context) {
+    super(props, context);
 
-  state = {
+    this.handleSelect = this.handleSelect.bind(this);
+
+    this.state = {
+      index: 0,
+      direction: null,
+    };
   }
 
+  handleSelect(selectedIndex, e) {
+    this.setState({
+      index: selectedIndex,
+      direction: e.direction,
+    });
+  }
+
+
     render() {
+      const { index, direction } = this.state;
+
       return (
         <Modal
         {...this.props}
@@ -26,11 +44,56 @@ class EtcModal extends Component {
           </Modal.Title>
         </Modal.Header>
         <Modal.Body style={{display:'flex',alignItems: 'center', flexDirection:'column'}}>
-          <img src={banners} alt ="DW" style={{width:'100%'}}/><br/>
-          <img src={parcpage} alt ="DW Shop" style={{width:'90%'}}/><br/>
-          <img src={duracrete} alt ="DW" style={{width:'60%'}}/><br/>
-          <img src={duracretemixer} alt ="DW Shop" style={{width:'60%'}}/><br/>
-          <img src={duracreteposter} alt ="DW Shop" style={{width:'60%'}}/><br/>
+         
+
+          <Carousel
+          activeIndex={index}
+          direction={direction}
+          onSelect={this.handleSelect}
+          interval={1000}
+          pauseOnHover={false}
+          >
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src={banners}
+                alt="First slide"
+              />
+            
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src={parcpage}
+                alt="Third slide"
+              />
+
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src={duracrete}
+                alt="Third slide"
+              />
+
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src={duracretemixer}
+                alt="Fourth slide"
+              />
+
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src={duracreteposter}
+                alt="Fifth slide"
+              />
+
+            </Carousel.Item>
+          </Carousel>
 
         </Modal.Body>
         
